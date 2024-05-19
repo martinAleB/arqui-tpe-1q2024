05317d33225b839983f46c817dadfd908b828631
martinAleB <martbarr@gmail.com>
2024-05-19 17:13:35 -0300

    Agrego comentarios en comandos bash

--- compile.sh
+++ compile.sh
@@ -1,3 +1,4 @@
+# Compila el proyecto a traves del container de Docker
 docker start tpe_arqui_1q2024
 docker exec -ti tpe_arqui_1q2024 make clean -C/root/Toolchain
 docker exec -ti tpe_arqui_1q2024 make clean -C/root
--- create-container.sh
+++ create-container.sh
@@ -1,1 +1,2 @@
+# Crea el container de Docker y setea la carpeta actual como compartida con el mismo
 docker run -d -v ${PWD}:/root --security-opt seccomp:unconfined -ti --name tpe_arqui_1q2024 agodio/itba-so:1.0
