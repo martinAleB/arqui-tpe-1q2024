8e3a4de6201c046110ab9dc85a6ffdef736698e3
leoWeitz <lweitz@Leow13>
2024-05-22 17:41:59 -0300

    soundDriver

---
+++ Kernel/c/drivers/soundDriver.c
@@ -0,0 +1,41 @@
+#include <soundDriver.h>
+#include <stdint.h>
+
+static void play_sound(uint32_t nFrequence) {
+ 	uint32_t Div;
+ 	uint8_t tmp;
+ 
+        //Seteamos el canal 2 del PIT para que tickee a una frecuencia que podemos escuchar 
+ 	Div = 1193180 / nFrequence;
+ 	outb(0x43, 0xb6);
+ 	outb(0x42, (uint8_t) (Div) );
+ 	outb(0x42, (uint8_t) (Div >> 8));
+ 
+
+        // "Activando" el bit 0 del puerto 61 linkeamos el canal 2 del PIT a los PC speakers, haciendo que cuando este "tickea"
+        // los speakers se muevan a la posición "in", y cuando este vuelve a ticker, los speakers se mueven a la posición "out".
+ 	tmp = inb(0x61);
+  	if (tmp != (tmp | 3)) {
+ 		outb(0x61, tmp | 3);
+ 	}
+ }
+
+ //"Desactiva" el bit 0 del puerto 61, desvinculando el PIT de los speakers
+ static void nosound() {
+ 	uint8_t tmp = inb(0x61) & 0xFC;
+ 
+ 	outb(0x61, tmp);
+ }
+
+static void timer_wait(int ticks) {
+	int now = ticks_elapsed();
+	while (ticks_elapsed() - now < ticks);
+	return;
+ }
+ 
+ //Hace un beep
+ void beep(int ticks) {
+ 	 play_sound(1000);
+ 	 timer_wait(3);
+ 	 nosound();
+ }
